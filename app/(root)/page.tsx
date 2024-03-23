fcf73f322a4c3e45d32c0e983349cea747ab19ed
Muhammad Kashir <kashir3160@gmail.com>
2024-03-23 17:05:59 +0500

    home page and onboarding design done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,8 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
+      {/* <UserButton afterSignOutUrl="/" /> */}
+      <h1 className="text-heading3-bold text-light-1">Home</h1>
     </div>
   );
 }
